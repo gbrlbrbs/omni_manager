be2346953fe376b6fb33fb94b3c4f74e6a23b02b
Gabriel Martinz <gabrielbarbosamartinz_@outlook.com>
2021-10-28 18:21:50 -0300

    [OM-11] I don't know what happened but it works

--- lib/main.dart
+++ lib/main.dart
@@ -5,58 +5,66 @@ import 'package:omni_manager/pages/Login.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'utils/constants.dart';
 
-Future main() async {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   Constants.prefs = await SharedPreferences.getInstance();
   runApp(App());
 }
 
-/// We are using a StatefulWidget such that we only create the [Future] once,
-/// no matter how many times our widget rebuild.
-/// If we used a [StatelessWidget], in the event where [App] is rebuilt, that
-/// would re-initialize FlutterFire and make our application re-enter loading state,
-/// which is undesired.
 class App extends StatefulWidget {
-  // Create the initialization Future outside of `build`:
-  @override
   _AppState createState() => _AppState();
 }
 
 class _AppState extends State<App> {
-  /// The future is part of the state of our widget. We should not call `initializeApp`
-  /// directly inside [build].
-  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+  // Set default `_initialized` and `_error` state to false
+  bool _initialized = false;
+  bool _error = false;
+
+  // Define an async function to initialize FlutterFire
+  void initializeFlutterFire() async {
+    try {
+      // Wait for Firebase to initialize and set `_initialized` state to true
+      await Firebase.initializeApp();
+      setState(() {
+        _initialized = true;
+      });
+    } catch(e) {
+      // Set `_error` state to true if Firebase initialization fails
+      setState(() {
+        _error = true;
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    initializeFlutterFire();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-      // Initialize FlutterFire:
-      future: _initialization,
-      builder: (context, snapshot) {
-        // Check for errors
-        if (snapshot.hasError) {
-          //return SomethingWentWrong();
-          // TODO: widget de erro
-          print("erro");
-        }
+    // Show error message if initialization failed
+    if(_error) {
+      return Container(
+          height: double.infinity,
+          width: double.infinity,
+          color: Color.fromRGBO(255, 36, 49, 1),
+          alignment: Alignment.bottomCenter,
+        );
+    }
 
-        // Once complete, show your application
-        if (snapshot.connectionState == ConnectionState.done) {
-          print('firebase init');
-          return MyApp();
-        }
-
-        // Otherwise, show something whilst waiting for initialization to complete
-        // TODO: criar widget de loading
-        // return Loading();
-        return Container(
+    // Show a loader until FlutterFire is initialized
+    if (!_initialized) {
+      return Container(
           height: double.infinity,
           width: double.infinity,
           color: Color.fromRGBO(34, 36, 49, 1),
           alignment: Alignment.bottomCenter,
         );
-      },
-    );
+    }
+
+    return MyApp();
   }
 }
 
