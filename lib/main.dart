ad2692900258c86558e21474a6fccb0b404dcc63
joao <vjoao3069@gmail.com>
2021-11-16 20:07:35 -0300

    forms page

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:omni_manager/pages/home.dart';
 import 'package:omni_manager/pages/login.dart';
 import 'package:omni_manager/pages/register.dart';
 import 'package:omni_manager/pages/settings.dart';
+import 'package:omni_manager/pages/forms.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'utils/constants.dart';  
 
@@ -96,6 +97,7 @@ class MyApp extends StatelessWidget {
         HomePage.routeName: (context) => HomePage(),
         RegisterPage.routeName: (context) => RegisterPage(),
         SettingsPage.routeName: (context) => SettingsPage(),
+        FormsPage.routeName: (context) => FormsPage(),
       },
     );
   }
