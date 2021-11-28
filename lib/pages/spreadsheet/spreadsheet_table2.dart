9810b98684af08e63d58b2e32088b5d05c830379
Gabriel Martinz <gabrielbarbosamartinz_@outlook.com>
2021-11-28 13:22:07 -0300

    [OM-33] naming and structure changes

--- lib/pages/spreadsheet/spreadsheet_table2.dart
+++ lib/pages/spreadsheet/spreadsheet_table2.dart
@@ -3,34 +3,34 @@ import 'package:flutter/material.dart';
 class DataTableWidget extends StatelessWidget {
   final List<Map<String, dynamic>> listOfColumns = [
     {
-      "media": "AAAAAA",
-      "atividade": "1",
+      "intensidade": "AAAAAA",
+      "atribuidas": 1,
+      "completadas": 1,
+      "taxa_completadas": 1/1,
       "presenca": "Yes",
-      "participacao": "AAAAAA",
       "proatividade": "1",
-      "solicitude": "Yes",
-      "media_2wks": "AAAAAA",
-      "nao_recebeu": "1"
+      "participacao": "AAAAAA",
+      "taxa_2wks": "AAAAAA",
     },
     {
-      "media": "BBBBBB",
-      "atividade": "2",
+      "intensidade": "BBBBBB",
+      "atribuidas": 2,
+      "completadas": 1,
+      "taxa_completadas": 1/2,
       "presenca": "no",
-      "participacao": "AAAAAA",
       "proatividade": "1",
-      "solicitude": "Yes",
-      "media_2wks": "AAAAAA",
-      "nao_recebeu": "1"
+      "participacao": "AAAAAA",
+      "taxa_2wks": "AAAAAA",
     },
     {
-      "media": "CCCCCC",
-      "atividade": "3",
-      "presenca": "Yes",
-      "participacao": "AAAAAA",
+      "intensidade": "CCCCCC",
+      "atribuidas": 3,
+      "completadas": 1,
+      "taxa_completadas": 1/3,
+      "qualidade": "Yes",
       "proatividade": "1",
-      "solicitude": "Yes",
-      "media_2wks": "AAAAAA",
-      "nao_recebeu": "1"
+      "participacao": "AAAAAA",
+      "taxa_2wks": "AAAAAA",
     }
   ];
 
@@ -40,28 +40,28 @@ class DataTableWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return DataTable(
       columns: [
-        DataColumn(label: Text('Média')),
-        DataColumn(label: Text('Fez atividade')),
-        DataColumn(label: Text('Presente na reunião')),
-        DataColumn(label: Text('Participação')),
+        DataColumn(label: Text('Intensidade de carga das tarefas')),
+        DataColumn(label: Text('Tarefas atribuídas')),
+        DataColumn(label: Text('Tarefas completadas')),
+        DataColumn(label: Text('Taxa de completude das tarefas')),
+        DataColumn(label: Text('Qualidade das entregas')),
         DataColumn(label: Text('Proatividade')),
-        DataColumn(label: Text('Solicitude')),
-        DataColumn(label: Text('Média últimas 2 semanas')),
-        DataColumn(label: Text('% de Não Recebeu Tarefas')),
+        DataColumn(label: Text('Participação')),
+        DataColumn(label: Text('Média de completude das últimas 2 semanas')),
       ],
       rows:
           listOfColumns // Loops through dataColumnText, each iteration assigning the value to element
               .map(
                 ((element) => DataRow(
                       cells: <DataCell>[
-                        DataCell(Text(element["media"]!)),
-                        DataCell(Text(element["atividade"]!)),
-                        DataCell(Text(element["presenca"]!)),
-                        DataCell(Text(element["participacao"]!)),
+                        DataCell(Text(element["intensidade"]!)),
+                        DataCell(Text(element["atribuidas"]!)),
+                        DataCell(Text(element["completadas"]!)),
+                        DataCell(Text(element["taxa_completadas"]!)),
+                        DataCell(Text(element["qualidade"]!)),
                         DataCell(Text(element["proatividade"]!)),
-                        DataCell(Text(element["solicitude"]!)),
-                        DataCell(Text(element["media_2wks"]!)),
-                        DataCell(Text(element["nao_recebeu"]!)),
+                        DataCell(Text(element["participacao"]!)),
+                        DataCell(Text(element["taxa_2wks"]!)),
                       ],
                     )),
               )
