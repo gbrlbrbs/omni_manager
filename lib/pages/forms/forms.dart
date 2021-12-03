0f94187ecd52e9b3bcf2ddf419509163ed76ab54
joao <vjoao3069@gmail.com>
2021-12-03 15:54:29 -0300

    solving problems with formulary display

--- lib/pages/forms/forms.dart
+++ lib/pages/forms/forms.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:omni_manager/pages/forms/widgets/formulary.dart';
-import 'package:omni_manager/pages/home.dart';
-import 'package:omni_manager/api/auth.dart';
+//import 'package:omni_manager/api/firebase.dart';
 
 import '../dashboard/widgets/custom_text_title.dart';
 
@@ -11,24 +10,61 @@ class FormsPage extends StatefulWidget {
   _FormsPageState createState() => _FormsPageState();
 }
 
+bool loggedUserIsManager =
+    true; //Import from firebase.dart Database in Dell's OM-35 code
+
 class _FormsPageState extends State<FormsPage> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-        mainAxisSize: MainAxisSize.min,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Padding(
-              padding: EdgeInsets.only(left: 15),
-              child: CustomTextTitle(
-                text: "Formularies",
-                size: 24,
-                weight: FontWeight.bold,
-              )),
-          Padding(
-            padding: const EdgeInsets.only(left: 15),
-            child: Container(alignment: Alignment.center, child: Formulary()),
-          ),
-        ]);
+    if (loggedUserIsManager)
+      return Scaffold(
+          body: Center(
+        child: ListView(
+          //mainAxisSize: MainAxisSize.min,
+          //crossAxisAlignment: CrossAxisAlignment.start,
+          shrinkWrap: true,
+          children: [
+            Padding(
+                padding: EdgeInsets.only(left: 15),
+                child: CustomTextTitle(
+                  text: "Formularies for Managers",
+                  size: 24,
+                  weight: FontWeight.bold,
+                )),
+            Padding(
+              padding: const EdgeInsets.only(left: 15),
+              child: Container(
+                alignment: Alignment.center,
+                child: Formulary(),
+              ),
+            ),
+          ],
+        ),
+      ));
+    else
+      return Scaffold(
+          body: Center(
+        child: ListView(
+          //mainAxisSize: MainAxisSize.min,
+          //crossAxisAlignment: CrossAxisAlignment.start,
+          shrinkWrap: true,
+          children: [
+            Padding(
+                padding: EdgeInsets.only(left: 15),
+                child: CustomTextTitle(
+                  text: "Formularies for Employees",
+                  size: 24,
+                  weight: FontWeight.bold,
+                )),
+            Padding(
+              padding: const EdgeInsets.only(left: 15),
+              child: Container(
+                alignment: Alignment.center,
+                child: Formulary(),
+              ),
+            ),
+          ],
+        ),
+      ));
   }
 }
